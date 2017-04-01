44f273e0b33a76a9e81c35d0b91e5bf44ef5d2c3
lvxiaoxin <lvxin1204@163.com>
2017-04-01 09:40:10 +0800

    Store previous code

---
+++ test.asm
@@ -0,0 +1,30 @@
+; ---------------------------------------------------------------------------------------
+; Created by: lvxin
+; Study ID: 201411212012
+; Subject: Assembly language
+; Date: 2016-12-29
+; Complete Date: 2017-01-02
+; Description: - Program: TEST
+;
+;I am lvxiaoxin.
+;I am lvxiaoxin.
+;I am lvxiaoxin.
+;I am lvxiaoxin.
+;I am lvxiaoxin.
+;I am lvxiaoxin.
+;
+;Hello world!
+;Hello world!
+;Hello world!
+;Hello world!
+;Hello world!
+;Hello world!
+;
+;It works.
+;It works.
+;It works.
+;It works.
+;It works.
+;It works.
+;It works.
+; ---------------------------------------------------------------------------------------
